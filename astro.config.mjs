e1e4bb79088f6473a60060ae9d175732f911e0e9
Negri234279 <rafalejo01@gmail.com>
2026-08-02 19:58:50 +0200

    chore: update package.json and add dependencies for Preact and Tailwind CSS
    - Updated project name and Node.js engine version in package.json
    - Added new scripts for formatting and starting the server
    - Included dependencies for Preact, Tailwind CSS, and their respective plugins
    - Added devDependencies for TypeScript types and Prettier plugins
    
    style: format index.astro for consistency
    
    - Adjusted indentation in index.astro for better readability
    
    feat: add global CSS file for Tailwind CSS
    
    - Created global.css to import Tailwind CSS styles
    
    fix: update tsconfig.json for Preact support
    
    - Added compiler options for JSX and Preact in tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'
+import preact from '@astrojs/preact'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [preact()],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    adapter: node({
+        mode: 'standalone',
+    }),
+})
